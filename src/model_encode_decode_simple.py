bb425187b1e5664e0296a2f69897f20f87602ea3
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-04-15 11:55:06 +0200

    updated simple model

--- src/model_encode_decode_simple.py
+++ src/model_encode_decode_simple.py
@@ -6,6 +6,7 @@ from s2cnn import s2_near_identity_grid
 from s2cnn import so3_integrate
 from s2_deconv import S2Deconvolution
 from s2_conv import S2Convolution
+from so3_conv import SO3Convolution
 
 # pylint: disable=R,C,E1101
 import torch
@@ -53,11 +54,15 @@ class ModelEncodeDecodeSimple(nn.Module):
     def __init__(self, bandwidth=30):
         super().__init__()
 
-        self.features = [2, 10]
-        self.bandwidths = [bandwidth, 30]
+        self.features = [2, 10, 20, 60, 1]
+        self.bandwidths = [bandwidth, 30, 20, 10]
 
         assert len(self.bandwidths) == len(self.features)
         grid_s2 = s2_near_identity_grid(n_alpha=6, max_beta=np.pi/160, n_beta=1)
+        grid_so3_1 = so3_near_identity_grid(n_alpha=6, max_beta=np.pi/16, n_beta=1, max_gamma=2*np.pi, n_gamma=6)
+        grid_so3_2 = so3_near_identity_grid(n_alpha=6, max_beta=np.pi/ 8, n_beta=1, max_gamma=2*np.pi, n_gamma=6)
+        grid_so3_3 = so3_near_identity_grid(n_alpha=6, max_beta=np.pi/ 4, n_beta=1, max_gamma=2*np.pi, n_gamma=6)
+
 
         self.convolutional = nn.Sequential(
             S2Convolution(
@@ -65,29 +70,45 @@ class ModelEncodeDecodeSimple(nn.Module):
                 nfeature_out = self.features[1],
                 b_in  = self.bandwidths[0],
                 b_out = self.bandwidths[1],
-                b_inverse= self.bandwidths[1],
+                b_inverse = self.bandwidths[1],
                 grid=grid_s2),
             nn.PReLU(),
-            nn.BatchNorm3d(self.features[1], affine=True)
+            nn.BatchNorm3d(self.features[1], affine=True),
+            SO3Convolution(
+                nfeature_in  = self.features[1],
+                nfeature_out = self.features[2],
+                b_in  = self.bandwidths[1],
+                b_out = self.bandwidths[2],
+                b_inverse = self.bandwidths[2],
+                grid=grid_so3_1),
+            nn.PReLU(),
+            nn.BatchNorm3d(self.features[2], affine=True),
+            SO3Convolution(
+                nfeature_in  = self.features[2],
+                nfeature_out = self.features[3],
+                b_in  = self.bandwidths[2],
+                b_out = self.bandwidths[3],
+                b_inverse = self.bandwidths[3],
+                grid=grid_so3_2),
+            nn.PReLU(),
+            nn.BatchNorm3d(self.features[2], affine=True),
         )
 
         self.deconvolutional = nn.Sequential(
-            S2Convolution(
-                nfeature_in  = self.features[1],
-                nfeature_out = 1,
-                b_in  = self.bandwidths[1],
-                b_out = self.bandwidths[1],
+            SO3Convolution(
+                nfeature_in  = self.features[3],
+                nfeature_out = self.features[4],
+                b_in  = self.bandwidths[3],
+                b_out = self.bandwidths[3],
                 b_inverse = self.bandwidths[0],
-                grid=grid_s2),
+                grid=grid_so3_3),
             nn.PReLU(),
-            nn.BatchNorm3d(1, affine=True)
+            nn.BatchNorm3d(self.features[4], affine=True),
         )
 
     def forward(self, x1):
         x_enc = self.convolutional(x1)  # [batch, feature, beta, alpha, gamma]
         #print(f"encoded x shape is {x_enc.shape}")
-        x_enc = so3_to_s2_integrate(x_enc)  # [batch, feature]
-        #print(f"integrated x shape is {x_enc.shape}")
         x_dec = self.deconvolutional(x_enc)  # [batch, feature, beta, alpha, gamma]
         #print(f"decoded x shape is {x_dec.shape}")
         x_dec = so3_to_s2_integrate(x_dec)  # [batch, feature]
