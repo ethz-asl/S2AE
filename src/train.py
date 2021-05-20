124196cc654c8282ca14cab88d2b2793a3f75665
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2021-05-20 10:44:05 +0000

    wip

--- src/train.py
+++ src/train.py
@@ -32,7 +32,7 @@ torch.backends.cudnn.benchmark = True
 print(f"Setting parameters...")
 bandwidth = 100
 learning_rate = 4.5e-3
-n_epochs = 20
+n_epochs = 30
 batch_size = 5
 num_workers = 32
 n_classes = 9
