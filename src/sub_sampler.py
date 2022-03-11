73015f87ceecec78028c5d6d7546b1e60af75032
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-11 15:56:56 +0100

    wip sampler

--- src/sub_sampler.py
+++ src/sub_sampler.py
@@ -8,12 +8,19 @@ class SubSampler:
         self.output_bw = output_bw
 
     def _compute_input_bw(sefl, input_data):
-        # assert input_data.shape
-        return 42
+        assert(input_data.ndim == 4)
+        return input_data.shape[3] / 2
 
     def compute_output_data(self):
         return 42
 
 
 if __name__ == '__main__':
-    print('foo')
+    test_input = np.random.rand(10, 3, 400, 400)
+    print('--- SubSampler Test Driver -------------------')
+    print(f'Input matrix shape is: {test_input.shape}')
+
+    sampler = SubSampler(test_input, 100)
+    assert(sampler.input_bw == 200)
+
+    print('Finished.')
