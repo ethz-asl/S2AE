9f89484bcfa4e43529c03a3c67827315137a4c12
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-03-11 17:42:44 +0100

    it does something

--- src/sub_sampler.py
+++ src/sub_sampler.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+from sphere import Sphere
+from dh_grid import DHGrid
+from tqdm.auto import tqdm
+
 class SubSampler:
 
     def __init__(self, input_data, output_bw):
@@ -9,10 +13,17 @@ class SubSampler:
 
     def _compute_input_bw(sefl, input_data):
         assert(input_data.ndim == 4)
-        return input_data.shape[3] / 2
+        return int(input_data.shape[3] / 2)
 
     def compute_output_data(self):
-        return 42
+        n_clouds = self.input_data.shape[0]
+        n_clouds = 10
+        out = np.zeros((n_clouds, 3, 2*self.output_bw, 2*self.output_bw))
+        for i in tqdm(range(n_clouds)):
+            grid, _ = DHGrid.CreateGrid(self.output_bw)
+            sph = Sphere(bw = self.input_bw, features = self.input_data[i, :, :, :])
+            out[i, :, :, :] = sph.sampleUsingGrid(grid)
+        return out
 
 
 if __name__ == '__main__':
@@ -22,5 +33,9 @@ if __name__ == '__main__':
 
     sampler = SubSampler(test_input, 100)
     assert(sampler.input_bw == 200)
+
+    sampled = sampler.compute_output_data()
+    print(f'sampled shape is {sampled.shape}')
+
 
     print('Finished.')
