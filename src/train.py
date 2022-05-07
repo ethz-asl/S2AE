fixing val computation

src/train.py
@@ -112,7 +112,7 @@ print(f"Shape clouds is {cloud_features.shape} and sem clouds is {sem_cloud_feat
 # --------------------------------------------------------------------
 
 # --- EXTERNAL SPLITTING ---------------------------------------------
-val_filename = f"{export_ds}/sem_clouds3_val_400.npy"
+val_filename = f"{export_ds}/val/sem_clouds_val_400.npy"
 
 print(f"Loading clouds from {val_filename}.")
 cloud_val = np.load(val_filename)
@@ -187,6 +187,9 @@ def validate_lidarseg(net, criterion, optimizer, writer, epoch, n_iter):
             writer.add_scalar('Validation/Loss', loss.mean().item(), n_iter)
 
             pred_segmentation = torch.argmax(enc_dec_cloud, dim=1)
+            mask = lidarseg_gt <= 0
+            pred_segmentation[mask] = 0
+            
             pixel_acc, pixel_acc_per_class, jacc, dice = eval_metrics(lidarseg_gt, pred_segmentation, num_classes = n_classes)
             avg_pixel_acc.update(pixel_acc)
             avg_pixel_acc_per_class.update(pixel_acc_per_class)
@@ -238,6 +241,9 @@ def test_lidarseg(net, criterion, writer):
             enc_dec_cloud = net(cloud)
 
             pred_segmentation = torch.argmax(enc_dec_cloud, dim=1)
+            mask = lidarseg_gt <= 0
+            pred_segmentation[mask] = 0
+            
             pixel_acc, pixel_acc_per_class, jacc, dice = eval_metrics(lidarseg_gt, pred_segmentation, num_classes = n_classes)
             avg_pixel_acc.update(pixel_acc)
             avg_pixel_acc_per_class.update(pixel_acc_per_class)