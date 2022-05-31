f9ae998dd79b30c326d2b700604103c258db9319
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-05-31 20:30:52 +0200

    viz updates

--- playground/matlab/viz.m
+++ playground/matlab/viz.m
@@ -1,3 +1,4 @@
+%% Improvement Figure
 % AVG Jaccard Coefficient
 jaccard_fusion = [0.6507, 0.6504, 0.6598, 0.6545, 0.6606, 0.6709, 0.6688, 0.671, 0.6716, 0.6773];
 jaccard_orig = [0.5045, 0.5409, 0.5528, 0.5772, 0.5813, 0.5938, 0.5964, 0.5964, 0.6008, 0.6113];
@@ -37,6 +38,53 @@ beautifySimplePlot('Fusion Improvement');
 prepareFig();
 set(gcf,'color','w');
 
+
+%% Performance Figure
+s2_projection_s = 0.015453020731608;
+s2_projection_cam_s = 0.0323154;
+dh_sampling_s = 0.16610;
+dh_sampling_cam_s = 0.16610;
+forward_pass_s = 0.0459082032995728;
+forward_pass_fusion_s = 0.0031;
+r3_back_projection = 0.01;
+
+X = categorical([1,2,3,4,5,6,7], 1:7, ...
+  {'$S^2$\,\textrm{Projection LiDAR}'; '$S^2$\,\textrm{Projection Visual}'; '\textrm{DH sampling LiDAR}'; '\textrm{DH sampling Visual}'; '\textrm{Inference Base}'; '\textrm{Inference Fusion}'; '\textrm{Backprojection}'},...
+  'Ordinal', true);
+Y = [s2_projection_s, s2_projection_cam_s, dh_sampling_s, dh_sampling_cam_s, forward_pass_s, forward_pass_fusion_s, r3_back_projection]; 
+
+% convert to ms
+Y = Y .* 1000;
+
+figure;
+b = bar(X,Y','FaceColor','flat');
+cm = colormap('gray');
+for k = 1:size(Y,2)
+  b.CData(k,:) = cm(k*10,:);  
+end
+
+
+beautifySimplePlot("")
+ylabel("\textrm{Execution Time}\,[\textrm{ms}]");
+% xlabel("\textrm{Neighbors}");
+% legend('Location', 'Best');
+
+prepareFig();
+set(gcf, 'color', 'w');
+set(gca, 'color', 'w');
+
+grid on;
+ax=gca;
+ax.YGrid=false;
+ax.XGrid=true;
+ax.GridAlpha = 0.75;
+ax.YMinorGrid=true;
+
+% yticklabels({'0','100','200','300','400','500','600'});
+
+disp(sprintf('Total time: %ds', sum(sum(Y))/100));
+
+%% Functions
 function beautifySimplePlot(name) 
       set(gca         , ...
         'FontName'    , 'Helvetica' );
@@ -55,5 +103,5 @@ function beautifySimplePlot(name)
         'XColor'      , [.3 .3 .3], ...
         'YColor'      , [.3 .3 .3], ...        
         'LineWidth'   , 1.5         );
-      title(name);
+%       title(name);
 end  
