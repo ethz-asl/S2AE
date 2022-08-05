3f0a0fcbc2ddef52a565ea383a2b63d150e7e306
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-08-05 13:16:19 +0000

    testing new training

--- src/s2_conv.py
+++ src/s2_conv.py
@@ -34,6 +34,8 @@ class S2Convolution(Module):
         :x:      [batch, feature_in,  beta, alpha]
         :return: [batch, feature_out, beta, alpha, gamma]
         '''
+        if (x.size(1) != self.nfeature_in):
+            print(f'ERROR: size mismatch: {x.size(1)} vs. {self.nfeature_in}')
         assert x.size(1) == self.nfeature_in
         assert x.size(2) == 2 * self.b_in
         assert x.size(3) == 2 * self.b_in
