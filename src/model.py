9ab214af6fe175cb589f69c139dbe06c3be3dfcb
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-08-16 16:24:40 +0000

    wip new training

--- src/model.py
+++ src/model.py
@@ -56,8 +56,8 @@ class Model(nn.Module):
         #self.bandwidths = [bandwidth, 60, 40, 30, 20, 10, 20, 30, 40, 60, bandwidth]
 
         # big model 5x24GB (batch size = 10)
-        self.features = [2, 20, 40, 80, 180, 80, 40, 20, n_classes]
-        self.bandwidths = [bandwidth, 50, 40, 20, 15, 10, 15, 20, 40, 50, bandwidth]
+#         self.features = [2, 20, 40, 80, 180, 80, 40, 20, n_classes]
+#         self.bandwidths = [bandwidth, 50, 40, 20, 15, 10, 15, 20, 40, 50, bandwidth]
 
         print(f'[Model] We have {self.features} features.' )
         print(f'[Model] We have {self.bandwidths} bandwidths.' )
@@ -109,7 +109,8 @@ class Model(nn.Module):
                 b_inverse = self.bandwidths[2],
                 grid=grid_so3_2),
             nn.BatchNorm3d(self.features[2], affine=True),
-            nn.PReLU()
+            nn.PReLU(),
+            nn.Dropout3d(p=0.05),
         )
         
         self.max_pool2 = SO3Pooling(self.bandwidths[2], self.bandwidths[3])
@@ -133,12 +134,12 @@ class Model(nn.Module):
                 grid=grid_so3_3),
             nn.BatchNorm3d(self.features[3], affine=True),
             nn.PReLU(),
+            nn.Dropout3d(p=0.05),
         )
         
         self.max_pool3 = SO3Pooling(self.bandwidths[3], self.bandwidths[4])
         
         self.conv4 = nn.Sequential(
-            nn.Dropout(p=0.4),
             SO3Convolution(
                 nfeature_in  = self.features[3],
                 nfeature_out = self.features[4],
@@ -157,7 +158,7 @@ class Model(nn.Module):
                 grid=grid_so3_4),
             nn.BatchNorm3d(self.features[4], affine=True),
             nn.PReLU(),
-            nn.Dropout(p=0.4),
+            nn.Dropout3d(p=0.05),
         )
         
         # -------------------------------------------------------------------------------
